75a17a1defb1a9cf5630ac3ca1f8ed9629978553
Luca Arienti <lucarienti@gmail.com>
2022-07-04 16:40:58 +0200

    feat: show/hide track footer

--- components/tracks/Footer/Index.jsx
+++ components/tracks/Footer/Index.jsx
@@ -12,22 +12,28 @@ export const TrackFooter = () => {
             : 0
     }, [track])
 
+    const isPlaying = useMemo(() => {
+        return !!track
+    }, [track])
+
     return (
-        <footer
-            className="bg-gray-50 border-t border-gray-300 h-32 fixed left-0 bottom-0 w-full px-8 flex items-center">
-            {track && (
-                <div className="w-full max-w-5xl mx-auto flex flex-col text-center">
-                    <dl>
-                        <dt className="sr-only">Title</dt>
-                        <dd className="text-gray-900 text-sm font-medium">{track.title}</dd>
-                        <dt className="sr-only">Album</dt>
-                        <dd className="text-gray-500 text-sm mb-1">{track.albumTitle}</dd>
-                        <dt className="sr-only">Duration</dt>
-                        <dd className="text-gray-600 text-xs font-bold mb-2">{durationInMinutes}</dd>
-                    </dl>
-                    <TrackProgress />
-                </div>
-            )}
-        </footer>
+            <footer
+                className={`bg-gray-50 border-t border-gray-300 h-32 fixed left-0 bottom-0 w-full px-8 flex items-center transition-transform duration-500
+                ${isPlaying ? 'translate-y-0' : 'translate-y-full'}`}
+            >
+                {isPlaying && (
+                    <div className="w-full max-w-5xl mx-auto flex flex-col text-center">
+                        <dl>
+                            <dt className="sr-only">Title</dt>
+                            <dd className="text-gray-900 text-sm font-medium">{track.title}</dd>
+                            <dt className="sr-only">Album</dt>
+                            <dd className="text-gray-500 text-sm mb-1">{track.albumTitle}</dd>
+                            <dt className="sr-only">Duration</dt>
+                            <dd className="text-gray-600 text-xs font-bold mb-2">{durationInMinutes}</dd>
+                        </dl>
+                        <TrackProgress/>
+                    </div>
+                )}
+            </footer>
     )
 }
